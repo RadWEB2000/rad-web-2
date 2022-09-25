578ef5e86cb1e4d5488acc234ab5629936533c17
RadoslawAdamczyk2000 <greatpolandweb@gmail.com>
2022-09-25 13:37:31 +0200

    remove fonts component

--- website/pages/_document.tsx
+++ website/pages/_document.tsx
@@ -1,7 +1,6 @@
 import Document, {Html,Head,Main,NextScript,DocumentContext,} from "next/document";
 import { ServerStyleSheet } from "styled-components";
-import { Fonts } from "../lib/fonts/fonts";
-  class MyDocument extends Document {
+class MyDocument extends Document {
     static async getInitialProps(ctx: DocumentContext) {
       const sheet = new ServerStyleSheet();
       const originalRenderPage = ctx.renderPage;
@@ -29,7 +28,13 @@ import { Fonts } from "../lib/fonts/fonts";
       return (
         <Html>
           <Head>
-            <Fonts/>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link
+              rel="preconnect"
+              href="https://fonts.gstatic.com"
+              crossOrigin="true"
+            />
+            <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400&display=swap" rel="stylesheet"/>
           </Head>
           <body>
             <Main />
