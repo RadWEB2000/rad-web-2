e5006c66b50e9cdbb475e5a1564a02e0228aa67b
RadoslawAdamczyk2000 <greatpolandweb@gmail.com>
2022-09-26 18:32:19 +0200

    create RadWEB FilmBase project post

--- website/data/posts.ts
+++ website/data/posts.ts
@@ -44,6 +44,16 @@ export const posts:IntPosts = {
     projects:[
         {
             category:{
+                name:'aplikacja',
+                value:'web'
+            },
+            content:'RadWEB FilmBase to aplikacja, która umożliwi Tobie szybkie oraz intuicyjne wyszukiwanie dowolonego filmu, serialu bądź obsadę aktorską',
+            image:'https://images.unsplash.com/photo-1559570278-eb8d71d06403?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=626&q=80',
+            path:'/projekty/radweb-filmbase',
+            title:'RadWEB FilmBase'
+        },
+        {
+            category:{
                 name:'strona internetowa',
                 value:'web'
             },
