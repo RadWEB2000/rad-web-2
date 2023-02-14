829d8de8f988abb22086b9f72eaa7369e4f9184a
RadoslawAdamczyk2000 <greatpolandweb@gmail.com>
2023-02-14 22:44:19 +0100

    update robots.txt

---
+++ audit-tools/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RadWEB SEO Tools</title>
+</head>
+<body>
+    <nav>
+        <div>
+            <h2>RadWEB SEO Tools</h2>
+
+        </div>
+    <menu>
+        <li>
+            <a href="">
+             Start  
+            </a>
+        </li>
+        <li>
+            <a href="">
+             GSC   
+            </a>
+        </li>
+        <li>
+            <a href="">
+                GA
+            </a>
+            <li>
+            <a href="">
+                GA UA
+            </a>
+        </li>
+        <li>
+            <a href="">
+                GA 4
+            </a>
+        </li>
+        </li>
+        <li>
+            <a href="">
+                Ahrefs
+            </a>
+        </li>
+        <li>
+            <a href="">
+                Semstorm
+            </a>
+        </li>
+        <li>
+            <a href="">
+                Screaming Frog
+            </a>
+        </li>
+        <li>
+            <a href="">
+                Generator dokumentów
+            </a>
+            <li>
+            <a href="">
+                Audyt
+            </a>
+        </li>
+            <li>
+            <a href="">
+                Raport
+            </a>
+        </li>
+        </li>
+    </menu>
+    <div id="settings">
+        <div class="" id="account">
+            <button>
+                konto
+            </button>
+        </div>
+        <div id="">
+            <button>
+                język
+            </button>
+            <button>
+motyw
+            </button>
+        </div>
+    </div>
+    </nav>
+    <main>
+l
+    </main>
+</body>
+</html>
---
+++ audit-tools/parts/navigation.php
@@ -0,0 +1,57 @@
+    <nav>
+        <div>
+            <h2>RadWEB SEO Tools</h2>
+        </div>
+    <menu>
+        <li>
+            <a href="">
+             GSC   
+            </a>
+        </li>
+        <li>
+            <a href="">
+                GA
+            </a>
+            <li>
+            <a href="">
+                GA UA
+            </a>
+        </li>
+        <li>
+            <a href="">
+                GA 4
+            </a>
+        </li>
+        </li>
+        <li>
+            <a href="">
+                Ahrefs
+            </a>
+        </li>
+        <li>
+            <a href="">
+                Semstorm
+            </a>
+        </li>
+        <li>
+            <a href="">
+                Screaming Frog
+            </a>
+        </li>
+        <li>
+            <a href="">
+                Generator dokumentów
+            </a>
+            <li>
+            <a href="">
+                Audyt
+            </a>
+        </li>
+            <li>
+            <a href="">
+                Raport
+            </a>
+        </li>
+        </li>
+    </menu>
+    </nav>
