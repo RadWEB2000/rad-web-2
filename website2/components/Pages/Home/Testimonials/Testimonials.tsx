045f59ebc33f1872255a944a62e1704f0716dc9c
RadoslawAdamczyk2000 <greatpolandweb@gmail.com>
2023-03-08 21:22:21 +0100

    remove no used imports

--- website2/components/Pages/Home/Testimonials/Testimonials.tsx
+++ website2/components/Pages/Home/Testimonials/Testimonials.tsx
@@ -1,7 +1,6 @@
 import SectionHomePage from "../../../Utilities/Sections/SectionHomePage/SectionHomePage";
 import styles from "./Testimonials.module.scss";
 import { IntTestimonials } from "./interface";
-import { IntTestimonialCard } from "./../../../Utilities/Cards/TestimonialCard/interface";
 import { ReactElement } from "react";
 import Masonry from "react-smart-masonry";
 import TestimonialCard from "../../../../src/components/Utilities/Cards/TestimonialCard/TestimonialCard";
@@ -15,21 +14,19 @@ const Testimonials = ({ cards }: IntTestimonials): ReactElement => {
 				columns={{ mobile: 1, tablet: 2, desktop: 3 }}
 				gap={{ mobile: 2.5, tablet: 5, desktop: 10 }}
 			>
-				{cards.map(
-					({ company, content, date, fullname, image }: IntTestimonialCard) => (
-						<TestimonialCard
-							company={company}
-							content={content}
-							date={date}
-							fullname={{
-								firstname: fullname.firstname,
-								lastname: fullname.lastname,
-							}}
-							image={image}
-							key={image}
-						/>
-					)
-				)}
+				{cards.map(({ company, content, date, fullname, image }) => (
+					<TestimonialCard
+						company={company}
+						content={content}
+						date={date}
+						fullname={{
+							firstname: fullname.firstname,
+							lastname: fullname.lastname,
+						}}
+						image={image}
+						key={image}
+					/>
+				))}
 			</Masonry>
 		</section>
 	);
