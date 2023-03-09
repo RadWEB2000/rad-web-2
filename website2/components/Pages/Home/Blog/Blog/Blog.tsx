bc8039b0829d0d6719a60b09c7ed4cee67f002be
RadoslawAdamczyk2000 <greatpolandweb@gmail.com>
2023-03-09 10:16:50 +0100

    update wrapper and blog cards in hp, create hook

--- website2/components/Pages/Home/Blog/Blog/Blog.tsx
+++ website2/components/Pages/Home/Blog/Blog/Blog.tsx
@@ -1,13 +1,44 @@
-import Cards from "../Cards/Cards";
+import BlogCard from "../../../../../src/components/Utilities/Cards/BlogCard/BlogCard";
+import CardsWrapper from "../../../../../src/components/Utilities/Cards/CardsWrapper/CardsWrapper";
 import SectionHomePage from "../../../../Utilities/Sections/SectionHomePage/SectionHomePage";
 import styles from "./Blog.module.scss";
-import { IntBlog } from "./interface";
+import { iArticle } from "../../../../../src/ts/interface";
 import { ReactElement } from "react";
-const Blog = ({ button, cards, content, title }: IntBlog): ReactElement => {
+import { useVisibleArticlesHP } from "../../../../../src/lib/hooks/useVisibleArticlesHP";
+interface iBlog {
+	button?: {
+		path: string;
+		title: string;
+	};
+	cards: iArticle[];
+	content: string;
+	title: string;
+}
+const Blog = ({ button, cards, content, title }: iBlog): ReactElement => {
 	return (
 		<div className={`section ${styles.wrapper}`}>
 			<SectionHomePage button={button} content={content} title={title} />
-			<Cards cards={cards} />
+			<CardsWrapper variant="articles">
+				{cards
+					.sort(
+						(a, b) =>
+							new Date(b.release).getTime() - new Date(a.release).getTime()
+					)
+					.slice(0, useVisibleArticlesHP())
+					.map((article: any) => {
+						const { category, excerpt, image, release, title, slug } = article;
+						return (
+							<BlogCard
+								release={release}
+								excerpt={excerpt}
+								image={image}
+								key={title}
+								path={`/blog/${category}/${slug}`}
+								title={title}
+							/>
+						);
+					})}
+			</CardsWrapper>
 		</div>
 	);
 };
