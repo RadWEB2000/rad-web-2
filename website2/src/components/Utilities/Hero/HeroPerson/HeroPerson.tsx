52e7267291cdf89d0857fc4efd099b842d2cab01
RadoslawAdamczyk2000 <greatpolandweb@gmail.com>
2023-03-10 10:17:15 +0100

    create desktop styles for hero person

--- website2/src/components/Utilities/Hero/HeroPerson/HeroPerson.tsx
+++ website2/src/components/Utilities/Hero/HeroPerson/HeroPerson.tsx
@@ -4,7 +4,7 @@ import styles from "./HeroPerson.module.scss";
 import { CgMail, CgSmartphone } from "react-icons/cg";
 import { motion } from "framer-motion";
 import { ReactElement } from "react";
-import { VscGlobe } from "react-icons/vsc";
+import { BiWorld } from "react-icons/bi";
 import getSocials from "../../../../lib/functions/getSocials";
 import { tFullname } from "../../../../ts/types";
 interface iHeroPerson {
@@ -29,7 +29,7 @@ const HeroPerson = ({
 }: iHeroPerson): ReactElement => {
 	return (
 		<header className={styles.wrapper}>
-			{/* <div className={styles.image}>
+			<div className={styles.image}>
 				<Image
 					alt={`${fullname.firstname} ${fullname.lastname} - ${jobs} - ${cities}`}
 					fill
@@ -40,73 +40,72 @@ const HeroPerson = ({
 					}}
 					title={`${fullname.firstname} ${fullname.lastname} - ${jobs} - ${cities}`}
 				/>
-			</div> */}
-			<div className={styles.content}>
-				<section>
-					<h1
-						className={styles.fullname}
-					>{`${fullname.firstname} ${fullname.lastname}`}</h1>
-					<h2 className={styles.jobs}>{jobs}</h2>
-					<h2 className={styles.cities}>{cities}</h2>
-					{socials && (
-						<ul className={styles.socials}>
-							{socials.map((item, index) => (
-								<li
-									className={styles.social}
-									key={index}
-									title={`${getSocials({ path: item }).name}`}
-								>
-									<Link
-										href={item}
-										title={`${getSocials({ path: item }).name}`}
-									>
-										{getSocials({ path: item }).icon}
-									</Link>
-								</li>
-							))}
-						</ul>
-					)}
-					{(email || phone) && (
-						<ul className={styles.contact}>
-							{phone && (
-								<li className={styles.contactItem}>
-									<Link href={`tel:${phone}`}>
-										<CgSmartphone />
-									</Link>
-								</li>
-							)}
-							{email && (
-								<li className={styles.contactItem}>
-									<Link href={`mailto:${email}`}>
-										<CgMail />
-									</Link>
-								</li>
-							)}
-						</ul>
-					)}
-					{website && (
-						<div className={styles.website}>
-							<Link
-								href={`${website}`}
-								legacyBehavior
-								rel="index follow"
-								target="_blank"
-							>
-								<motion.a
-									className={styles.websiteButton}
-									whileTap={{
-										scale: 0.9,
-									}}
-								>
-									<span>
-										<VscGlobe />
-									</span>
-								</motion.a>
-							</Link>
-						</div>
-					)}
-				</section>
 			</div>
+			<section className={styles.content}>
+				<h1
+					className={styles.fullname}
+				>{`${fullname.firstname} ${fullname.lastname}`}</h1>
+				<h2 className={styles.jobs}>{jobs}</h2>
+				<h2 className={styles.cities}>{cities}</h2>
+				{socials && (
+					<ul className={styles.socials}>
+						{socials.map((item, index) => (
+							<li
+								className={styles.social}
+								key={index}
+								title={`${getSocials({ path: item }).name}`}
+							>
+								<Link href={item} title={`${getSocials({ path: item }).name}`}>
+									{getSocials({ path: item }).icon}
+								</Link>
+							</li>
+						))}
+					</ul>
+				)}
+				{(email || phone || website) && (
+					<ul className={styles.contact}>
+						{phone && (
+							<motion.li
+								className={styles.contactItem}
+								whileTap={{
+									scale: 0.9,
+								}}
+							>
+								<Link href={`tel:${phone}`}>
+									<CgSmartphone />
+								</Link>
+							</motion.li>
+						)}
+						{website && (
+							<motion.li
+								className={styles.contactItem}
+								whileTap={{
+									scale: 0.9,
+								}}
+							>
+								<Link href={`${website}`} rel="index follow" target="_blank">
+									<span>
+										<BiWorld />
+									</span>
+								</Link>
+							</motion.li>
+						)}
+
+						{email && (
+							<motion.li
+								className={styles.contactItem}
+								whileTap={{
+									scale: 0.9,
+								}}
+							>
+								<Link href={`mailto:${email}`}>
+									<CgMail />
+								</Link>
+							</motion.li>
+						)}
+					</ul>
+				)}
+			</section>
 		</header>
 	);
 };
