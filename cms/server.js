373e4ef46a59406178832e3f168d3377abf67e46
RadoslawAdamczyk2000 <greatpolandweb@gmail.com>
2023-03-16 19:07:16 +0100

    configure express

--- cms/server.js
+++ cms/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const next = require("next");
-const app = next({ dev: process.env.NODE_ENV !== "production" });
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
 const handle = app.getRequestHandler();
 const expressApp = require("./pages/api/express.js");
+const apiRoutes = require("./routes/apiRoutes");
 
 app.prepare().then(() => {
 	const server = express();
@@ -12,4 +14,8 @@ app.prepare().then(() => {
 	server.listen(3000, () => {
 		console.log("Server listening on port 3000");
 	});
+});
+
+app.get(apiRoutes.hello, (req, res) => {
+	res.send({ message: "Hello from Express.js!" });
 });
