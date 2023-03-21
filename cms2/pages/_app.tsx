6192c548898f0eed1ebfda22155808fe70c35311
RadoslawAdamczyk2000 <greatpolandweb@gmail.com>
2023-03-21 20:32:18 +0100

    create context

--- cms2/pages/_app.tsx
+++ cms2/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@default/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { LayoutProvider } from "@default/src/context/LayoutProvider/LayoutProvider";
+// import "@default/styles/globals.css";
+import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => {
+	return (
+		<LayoutProvider>
+			<Component {...pageProps} />
+		</LayoutProvider>
+	);
+};
+
+export default App;
---
+++ cms2/src/components/Layout/Layout/Layout.tsx
@@ -0,0 +1,29 @@
+import Head from "next/head";
+import { ReactElement } from "react";
+
+export interface iLayout {
+	children: ReactElement | ReactElement[];
+	isLoginPage?: boolean;
+}
+
+const Layout = ({ children, isLoginPage = true }: iLayout) => {
+	return (
+		<>
+			<Head>
+				<title>RadWEB CMS</title>
+			</Head>
+			<>
+				{!isLoginPage ? (
+					<nav>
+						<li>
+							<a href="#">start</a>
+						</li>
+					</nav>
+				) : null}
+				<main>{children}</main>
+			</>
+		</>
+	);
+};
+
+export default Layout;
