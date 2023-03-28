1f1121146662bf6d4c75c8016ec353e20a0eb4bf
RadoslawAdamczyk2000 <greatpolandweb@gmail.com>
2023-03-28 11:23:24 +0200

    create scroll animations

--- website2/src/components/Pages/HomePage/WhyUsSection/WhyUsSection.tsx
+++ website2/src/components/Pages/HomePage/WhyUsSection/WhyUsSection.tsx
@@ -1,4 +1,5 @@
 import styles from "./WhyUsSection.module.scss";
+import { motion } from "framer-motion";
 import { ReactElement } from "react";
 
 interface iWhyUsSectionCard {
@@ -23,33 +24,115 @@ const WhyUsSectionCard = ({
 }: iWhyUsSectionCard): ReactElement => {
 	return (
 		<li className={styles.card}>
-			<span>{count}</span>
-			<h3>{title}</h3>
-			<p dangerouslySetInnerHTML={{ __html: content }} />
+			<motion.span
+				initial={{
+					y: -50,
+					opacity: 0,
+					visibility: "hidden",
+				}}
+				whileInView={{
+					y: 0,
+					opacity: 1,
+					visibility: "visible",
+					transition: {
+						duration: 1.25,
+					},
+				}}
+			>
+				{count}
+			</motion.span>
+			<motion.h3
+				initial={{
+					x: -50,
+					opacity: 0,
+					visibility: "hidden",
+				}}
+				whileInView={{
+					x: 0,
+					opacity: 1,
+					visibility: "visible",
+					transition: {
+						duration: 1.25,
+					},
+				}}
+			>
+				{title}
+			</motion.h3>
+			<motion.p
+				dangerouslySetInnerHTML={{ __html: content }}
+				initial={{
+					y: 50,
+					opacity: 0,
+					visibility: "hidden",
+				}}
+				whileInView={{
+					y: 0,
+					opacity: 1,
+					visibility: "visible",
+					transition: {
+						duration: 1.25,
+					},
+				}}
+			/>
 		</li>
 	);
 };
 
 const WhyUsSection = ({ cards, content, title }: iWhyUsSection) => {
 	return (
-		<div className={styles.wrapper}>
-			<section>
-				<h2>{title}</h2>
-				<p dangerouslySetInnerHTML={{ __html: content }} />
-			</section>
-			{cards && (
-				<ul>
-					{cards.map(({ content, title }, index) => (
-						<WhyUsSectionCard
-							content={content}
-							count={`0${index + 1}`}
-							key={title}
-							title={title}
-						/>
-					))}
-				</ul>
-			)}
-		</div>
+		<>
+			<div className={styles.wrapper}>
+				<section>
+					<motion.h2
+						initial={{
+							opacity: 0,
+							visibility: "hidden",
+							x: -50,
+						}}
+						whileInView={{
+							opacity: 1,
+							visibility: "visible",
+							x: 0,
+							transition: {
+								duration: 0.5,
+								delay: 0.15,
+							},
+						}}
+					>
+						{title}
+					</motion.h2>
+					<motion.p
+						dangerouslySetInnerHTML={{ __html: content }}
+						initial={{
+							opacity: 0,
+							visibility: "hidden",
+							y: -50,
+						}}
+						whileInView={{
+							opacity: 1,
+							visibility: "visible",
+							y: 0,
+							transition: {
+								duration: 0.67,
+								delay: 0.15,
+							},
+						}}
+					/>
+				</section>
+				{cards && (
+					<ul>
+						{cards.map(({ content, title }, index) => (
+							<WhyUsSectionCard
+								content={content}
+								count={`0${index + 1}`}
+								key={title}
+								title={title}
+							/>
+						))}
+					</ul>
+				)}
+			</div>
+		</>
 	);
 };
 export default WhyUsSection;
