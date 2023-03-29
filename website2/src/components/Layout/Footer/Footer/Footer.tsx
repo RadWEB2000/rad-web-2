a483b75ef37ea9fc52a4ad9a75cca2e8f5f138d1
RadoslawAdamczyk2000 <greatpolandweb@gmail.com>
2023-03-29 21:13:06 +0200

    create desktop styles

--- website2/src/components/Layout/Footer/Footer/Footer.tsx
+++ website2/src/components/Layout/Footer/Footer/Footer.tsx
@@ -1,11 +1,11 @@
-import Author from "../Author/Author";
-import Info from "../Details/Info/Info";
-import Map from "../Map/Map";
-import Menu from "../Menu/Menu";
-import Socials from "../Details/Socials/Socials";
 import styles from "./Footer.module.scss";
 import { menu } from "../../../../data/menu";
 import Link from "next/link";
+import { footer as footerData } from "../../../../../data/footer";
+import getSocials from "../../../../../lib/getSocials";
+import { IoHomeSharp, IoMailOpenOutline } from "react-icons/io5";
+import { BsTelephoneFill } from "react-icons/bs";
+
 interface iFooter {
 	info: {
 		address: string;
@@ -18,70 +18,72 @@ interface iFooter {
 const Footer = ({ info, socials }: iFooter) => {
 	const { footer, settings } = menu.pl;
 	return (
-		<>
-			{/* <Map />
-			<footer className={styles.wrapper}>
-				<div className={styles.content}>
-					<div className={styles.menu}>
-						<Menu />
-					</div>
-					<div className={styles.details}>
-						<Socials socials={socials} />
-						<Info
-							address={info.address}
-							nip={info.nip}
-							regon={info.regon}
-							title={info.title}
-						/>
-					</div>
-				</div>
-				<Author />
-			</footer> */}
-			<footer className={styles.footer2}>
-				<section>
-					<h2>RadWEB</h2>
+		<footer className={styles.wrapper}>
+			<section className={styles.info}>
+				<h2>RadWEB</h2>
+				<p>
+					Consequatur laborum earum quia nulla recusandae assumenda illum.
+					Facere veritatis mollitia rerum perferendis ea fuga. Repudiandae neque
+					cupiditate commodi et laboriosam consequatur odit.
+				</p>
+				<address>
 					<p>
-						Consequatur laborum earum quia nulla recusandae assumenda illum.
-						Facere veritatis mollitia rerum perferendis ea fuga. Repudiandae
-						neque cupiditate commodi et laboriosam consequatur odit.
+						<i>
+							<IoHomeSharp />
+						</i>
+						<span>Donatowo 54, 64-020 Czempiń</span>
 					</p>
-					<address>
-						<p>Donatowo 54, 64-020 Czempiń</p>
-						<p>+48 794-100-413</p>
-						<p>radoslaw.adamczyk2000@gmail.com</p>
-					</address>
-				</section>
-				<menu>
-					{footer.map(({ columnName, pages }) => (
-						<div key={columnName}>
-							<li>
-								<h3>{columnName}</h3>
-								<ul>
-									{pages.map(({ path, title }) => (
-										<li key={path}>
-											<Link href={path}>{title}</Link>
-										</li>
-									))}
-								</ul>
-							</li>
-						</div>
-					))}
-				</menu>
-				<div>
-					<p>Radosław Adamczyk &copy; 2023</p>
-					<div>
-						<ul></ul>
+					<Link href="tel:+48794100413">
+						<i>
+							<BsTelephoneFill />
+						</i>
+						<span>+48 794-100-413</span>
+					</Link>
+					<Link href="mailto:radoslaw.adamczyk2000@gmail.com">
+						<i>
+							<IoMailOpenOutline />
+						</i>
+						<span>radoslaw.adamczyk2000@gmail.com</span>
+					</Link>
+				</address>
+			</section>
+			<menu className={styles.menu}>
+				{footer.map(({ columnName, pages }) => (
+					<li key={columnName}>
+						<h3>{columnName}</h3>
 						<ul>
-							{settings.map(({ path, title }) => (
-								<li key={path}>
+							{pages.map(({ path, title }) => (
+								<li className={styles.link} key={path}>
 									<Link href={path}>{title}</Link>
 								</li>
 							))}
 						</ul>
-					</div>
+					</li>
+				))}
+			</menu>
+			<span className={styles.break} />
+			<div className={styles.settings}>
+				<p>Radosław Adamczyk - RadWEB &copy; 2021-2023</p>
+				<div>
+					<ul className={styles.socials}>
+						{footerData.pl.socials.map((item, index) => (
+							<li key={`${index}`} title={`${getSocials({ path: item }).name}`}>
+								<Link href={item} title={`${getSocials({ path: item }).name}`}>
+									{getSocials({ path: item }).icon}
+								</Link>
+							</li>
+						))}
+					</ul>
+					<ul className={styles.menu}>
+						{settings.map(({ path, title }) => (
+							<li className={styles.link} key={path}>
+								<Link href={path}>{title}</Link>
+							</li>
+						))}
+					</ul>
 				</div>
-			</footer>
-		</>
+			</div>
+		</footer>
 	);
 };
 export default Footer;
