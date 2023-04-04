c18b7f8436e5afa4a9c06fa7136bf3add2296236
RadoslawAdamczyk2000 <greatpolandweb@gmail.com>
2023-04-04 11:48:51 +0200

    update contnet

---
+++ website2/content/pages/aboutpage.tsx
@@ -0,0 +1,76 @@
+import { ReactElement } from "react";
+import { FaHandsHelping } from "react-icons/fa";
+import { RiPenNibLine, RiPlantFill } from "react-icons/ri";
+import { iSeoPage } from "../../src/ts/interface";
+
+interface iAboutpageElement {
+	seo: iSeoPage;
+	hero: {
+		title: string;
+		content: string;
+	};
+	mission: {
+		title: string;
+		content: string;
+		cards?: {
+			title: string;
+			content: string;
+			icon: ReactElement | JSX.Element;
+		}[];
+	};
+	team: {
+		title: string;
+		content: string;
+	};
+}
+
+interface iAboutpage {
+	pl: iAboutpageElement;
+}
+
+export const aboutpage: iAboutpage = {
+	pl: {
+		seo: {
+			image:
+				"https://images.unsplash.com/photo-1473679408190-0693dd22fe6a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+			meta: {
+				description:
+					"Młodzi i głodni sukcesów. Poszukujemy nowych wyzwań, przez które, szkolimy swoje umiejętności. Chcesz być częścią sukcesu? Poznaj specjalistów RadWEB!",
+				title: "Poznaj specjalistów od internetu, zespoł RadWEB - RadWEB",
+			},
+			og: {
+				description: "Młodzi i głodni sukcesów. Poznaj zespół RadWEB!",
+				title: "Poznaj swoich specjalistów",
+			},
+		},
+		hero: {
+			title: "Kim jesteśmy?",
+			content: `Jesteśmy grupą znajomych z całej Polski, której głównym zainteresowaniem jest technologia. Realizujemy swoje hobby w zakresie UIi/UX stron internetowych, tworzenia stron internetowych, pozycjonowania serwisów internetowych, copywritingu oraz tworzeniu treści wideo.`,
+		},
+		mission: {
+			title: "Misja RadWEB",
+			content: `W RadWEB wierzymy, że nasz sukces zależy od naszej uczciwości, profesjonalizmu i ciągłego rozwoju. Nasz zespół składa się z ludzi, którzy dążą do doskonałości w każdym aspekcie swojej pracy i są zaangażowani w dostarczanie naszym klientom najlepszych możliwych rozwiązań.<br/><br/> Jesteśmy dumni z naszych usług w zakresie tworzenia stron internetowych, pozycjonowania stron internetowych, projektowania stron internetowych, copywritingu i videomaking. Nasze projekty są zawsze dostosowane do potrzeb naszych klientów i są tworzone z najwyższą starannością, aby zapewnić naszym klientom nie tylko świetnie wyglądające strony internetowe, ale również strony internetowe, które działają skutecznie i efektywnie.`,
+			cards: [
+				{
+					icon: <FaHandsHelping />,
+					title: "Uczciwość",
+					content: `Uczciwość to jedna z cech, która nas wyróżnia. Stawiamy na transparentność oraz na terminową realizację usług.`,
+				},
+				{
+					icon: <RiPenNibLine />,
+					title: "Profesjonalizm",
+					content: `Wszystkie nasze projekty są realizowane zgodnie z wolą klienta oraz najlepszymi praktykami.`,
+				},
+				{
+					icon: <RiPlantFill />,
+					title: "Rozwój",
+					content: `Każdy projekt to możliwość rozwoju. Rozwijamy swoje umiejętności codziennie, aby móc oferować najwyższej jakości usługi.`,
+				},
+			],
+		},
+		team: {
+			title: "Zespół RadWEB",
+			content: `RadWEB znajdziesz w wielu lokalizacjach w Polsce, w tym w Białej Podlaskiej, Bydgoszczy, Krakowie, Łodzi, Poznaniu, Warszawie i Wrocławiu. Dzięki temu jesteśmy w stanie świadczyć nasze usługi na terenie całego kraju, dostarczając naszym klientom nie tylko wysokiej jakości projekty, ale również pełen zakres usług, w tym pozycjonowanie serwisów internetowych, copywriting oraz tworzenie treści wideo.<br/><br/>Nasz zespół składa się z doświadczonych projektantów, programistów i marketerów, którzy współpracują ze sobą, aby zapewnić naszym klientom najlepsze możliwe rezultaty. Nasze projekty są zawsze dostosowane do potrzeb naszych klientów i są tworzone zgodnie z najnowszymi standardami branży, co gwarantuje, że nasze strony internetowe są zawsze nowoczesne i skuteczne.`,
+		},
+	},
+};
---
+++ website2/content/pages/contactpage.ts
@@ -0,0 +1,152 @@
+import { iSeoPage } from "../../src/ts/interface";
+
+interface iFormField {
+	id: string;
+	isTextarea?: boolean;
+	label: string;
+	max?: number;
+	min?: number;
+	name: string;
+	pattern?: string | RegExp;
+	placeholder: string;
+	required?: boolean;
+	type?: "text" | "email" | "tel" | "number";
+}
+
+interface iContactPageElement {
+	seo: iSeoPage;
+	content: {
+		title: string;
+		content: string;
+	};
+	form: {
+		fullname: iFormField;
+		email: iFormField;
+		phone: iFormField;
+		subject: iFormField;
+		message: iFormField;
+		button: string;
+		rodo: string;
+		title: string;
+		alerts: {
+			done: string;
+			error: string;
+			warning: string;
+		};
+	};
+	details: {
+		address: string;
+		content: string;
+		email: {
+			address: string;
+			name: string;
+		};
+		nip?: string | number;
+		phone: {
+			address: string;
+			name: string;
+		};
+		regon?: string | number;
+	};
+}
+
+interface iContactPage {
+	pl: iContactPageElement;
+}
+
+export const contactpage: iContactPage = {
+	pl: {
+		seo: {
+			image:
+				"https://images.unsplash.com/photo-1512428559087-560fa5ceab42?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+			meta: {
+				title: "Kontakt ze specjalistami - RadWEB",
+				description: `Jakiekolwiek działania związane ze stronami www, wymagają ogromnej wiedzy. Działań związanych z internetem nie pozostawiaj przypadkowi. Wybierz specjalistów.`,
+			},
+			og: {
+				title: "Kontakt ze specjalistami - RadWEB",
+				description: `Jakiekolwiek działania związane ze stronami www, wymagają ogromnej wiedzy. Działań związanych z internetem nie pozostawiaj przypadkowi. Wybierz specjalistów.`,
+			},
+		},
+		content: {
+			title: "Zaufaj specjalistom",
+			content: `Jeśli chcesz mieć pewność, że Twoja strona internetowa będzie funkcjonalna, estetyczna i skuteczna, to warto powierzyć ją w ręce specjalistów. W RadWEB mamy wieloletnie doświadczenie w projektowaniu i tworzeniu stron internetowych oraz aplikacji, dlatego jesteśmy w stanie zapewnić Ci najlepsze rozwiązania. Nie tylko realizujemy projekty, ale również dbamy o to, aby strony były jak najlepiej widoczne w wyszukiwarkach internetowych.<br/><br/> Dzięki wiedzy i umiejętnościom naszych specjalistów z zakresu pozycjonowania stron, będziesz mieć pewność, że Twoja strona znajdzie się na pierwszej stronie wyników wyszukiwania.Jeśli chcesz wyróżnić się na tle konkurencji i zaprezentować swoją ofertę w sposób atrakcyjny i przejrzysty, to nasza oferta jest skierowana właśnie do Ciebie. Nasi specjaliści z zakresu copywritingu pomogą Ci stworzyć treści, które zachęcą do skorzystania z Twoich usług.<b>W RadWEB jesteśmy zawsze gotowi do pomocy, dlatego nie wahaj się i napisz do nas już teraz. Z chęcią odpowiemy na wszystkie Twoje pytania i pomożemy Ci w realizacji Twojego projektu. Nie czekaj, skorzystaj z naszej oferty już dziś!</b>`,
+		},
+		form: {
+			fullname: {
+				id: "fullname",
+				isTextarea: false,
+				label: "Imię i nazwisko",
+				max: 50,
+				min: 10,
+				name: "fullname",
+				placeholder: "Wpisz tutaj",
+				type: "text",
+				required: true,
+			},
+			email: {
+				id: "email",
+				isTextarea: false,
+				label: "Adres e-mail",
+				max: 50,
+				min: 7,
+				name: "email",
+				pattern: `[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}`,
+				placeholder: "Wpisz tutaj",
+				required: true,
+				type: "email",
+			},
+			phone: {
+				id: "phone",
+				isTextarea: false,
+				label: "Numer telefonu",
+				max: 18,
+				min: 9,
+				name: "phone",
+				pattern: `[0-9]{3}-[0-9]{3}-[0-9]{4}`,
+				placeholder: "Wpisz tutaj",
+				required: true,
+				type: "tel",
+			},
+			subject: {
+				id: "subject",
+				isTextarea: false,
+				label: "Temat",
+				min: 10,
+				name: "subject",
+				placeholder: "Wpisz tutaj",
+				required: true,
+				type: "text",
+			},
+			message: {
+				id: "message",
+				isTextarea: true,
+				label: "Wiadomość",
+				min: 10,
+				name: "message",
+				placeholder: "Wpisz tutaj",
+				required: true,
+			},
+			alerts: {
+				done: "Wiadomość wysłana",
+				error: "Popraw formularz",
+				warning: "Poprawnie uzupełnij formularz",
+			},
+			button: "Wyślij",
+			rodo: `Poznałem i akceptuję <a href="#">Politykę prywatności</a>.`,
+			title: "Formularz kontaktowy",
+		},
+		details: {
+			address: `Donatowo 53<br/>64-020 Czempiń`,
+			content: `Strony internetowe, aplikacje webowe, pozycjonowanie stron internetowych, copywriting oraz tworzenie treści wideo. Tym zajmuje się RadWEB od 2021 roku. Zaufaj nam, a osiągniesz sukces!`,
+			email: {
+				address: "radoslaw.adamczyk2000@gmail.com",
+				name: "E-mail",
+			},
+			phone: {
+				address: "+48794100413",
+				name: "Telefon",
+			},
+		},
+	},
+};
