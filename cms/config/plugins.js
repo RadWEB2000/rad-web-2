323179bbbf8203a1db7bd5a723aa1773dd232ea7
RadoslawAdamczyk2000 <greatpolandweb@gmail.com>
2023-05-03 20:21:38 +0200

    cms2

---
+++ cms/config/plugins.js
@@ -0,0 +1,13 @@
+module.exports = ({ env }) => ({
+  "vercel-deploy": {
+    enabled: true,
+    config: {
+      deployHook:
+        "https://api.vercel.com/v1/integrations/deploy/prj_<deploy-hook>",
+      apiToken: "<vercel-api-token>",
+      appFilter: "your-app-name-on-vercel",
+      teamFilter: "your-team-id-on-vercel",
+      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+    },
+  },
+});
