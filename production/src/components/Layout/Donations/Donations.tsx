dfa74ab99bb298de5cae9ce5434a42ad9092bfa0
RadoslawAdamczyk2000 <greatpolandweb@gmail.com>
2023-08-28 11:15:58 +0200

    add styles

--- production/src/components/Layout/Donations/Donations.tsx
+++ production/src/components/Layout/Donations/Donations.tsx
@@ -1,6 +1,7 @@
+import styles from "@default/components/Layout/Donations/Donations.module.scss";
 export default function Donations() {
 	return (
-		<div>
+		<div className={styles.wrapper}>
 			<iframe src="https://zrzutka.pl/wpxcyt/widget/13"></iframe>
 		</div>
 	);
