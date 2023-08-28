f1efdca08639d7a7a2fcedb8afd5943aa82088f6
RadoslawAdamczyk2000 <greatpolandweb@gmail.com>
2023-08-28 11:22:24 +0200

    add title attribiute

--- production/src/components/Layout/Donations/Donations.tsx
+++ production/src/components/Layout/Donations/Donations.tsx
@@ -2,7 +2,10 @@ import styles from "@default/components/Layout/Donations/Donations.module.scss";
 export default function Donations() {
 	return (
 		<div className={styles.wrapper}>
-			<iframe src="https://zrzutka.pl/wpxcyt/widget/13"></iframe>
+			<iframe
+				src="https://zrzutka.pl/wpxcyt/widget/13"
+				title="Pomóżmy Radosławowi i jego babci zachować dom dziadków!"
+			/>
 		</div>
 	);
 }
