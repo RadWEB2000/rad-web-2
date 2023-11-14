e19b6f5258ff875eaf8c501f2588964dd937ce00
RadoslawAdamczyk2000 <greatpolandweb@gmail.com>
2023-11-14 19:53:29 +0100

    add styles to search button

--- dev4/src/components/Layout/Navigation/Settings/SearchButton/SearchButton.tsx
+++ dev4/src/components/Layout/Navigation/Settings/SearchButton/SearchButton.tsx
@@ -1,8 +1,9 @@
 import { FaSearch } from "react-icons/fa";
+import styles from "l_navigation/Settings/SearchButton/SearchButton.module.scss";
 
 export default function SearchButton() {
 	return (
-		<button>
+		<button className={styles.wrapper}>
 			<FaSearch />
 		</button>
 	);
