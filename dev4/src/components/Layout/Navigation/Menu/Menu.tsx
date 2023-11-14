bf681c306a54ab61800070da3b8eb3c641652e7e
RadoslawAdamczyk2000 <greatpolandweb@gmail.com>
2023-11-14 17:25:31 +0100

    add nav file

--- dev4/src/components/Layout/Navigation/Menu/Menu.tsx
+++ dev4/src/components/Layout/Navigation/Menu/Menu.tsx
@@ -1,19 +1,14 @@
-import { tMenuArrayItem } from 'data/menu';
-import Item from './Item';
-import Submenu from './Submenu';
+import { tMenuArrayItem } from "data/menu";
+import Item from "./Item";
+import Submenu from "./Submenu";
 
 type tMenu = {
 	menu: tMenuArrayItem[];
 };
 
 export default function Menu(props: tMenu) {
-	console.log(props.menu);
 	return (
-		<div
-			style={{
-				color: 'red',
-			}}
-		>
+		<menu>
 			{props.menu.map(
 				({ label, uri, submenu, submenuTheme, submenuTitles }) => {
 					if (submenu !== null) {
@@ -22,23 +17,16 @@ export default function Menu(props: tMenu) {
 								label={label}
 								key={label}
 								submenu={submenu}
-								submenuTheme={submenuTheme || 'small'} // Użyj submenuTheme z menu
+								submenuTheme={"small"} // Użyj submenuTheme z menu
 								submenuTitle={submenuTitles || {}} // Użyj submenuTitles z menu
 								uri={uri}
 							/>
 						);
 					} else {
-						return (
-							<Item
-								label={label}
-								key={label}
-								theme="regural"
-								uri={uri}
-							/>
-						);
+						return <Item label={label} key={label} theme="regural" uri={uri} />;
 					}
 				}
 			)}
-		</div>
+		</menu>
 	);
 }
---
+++ dev4/src/components/Layout/Navigation/Navigation.tsx
@@ -0,0 +1,9 @@
+export default function Navigation() {
+	return (
+		<nav>
+			<div className="brand"></div>
+			<div className="menu"></div>
+			<div className="settings"></div>
+		</nav>
+	);
+}
