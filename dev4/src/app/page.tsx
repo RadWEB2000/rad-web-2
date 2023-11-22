2168ce52d9b71ea5c7a1c936f154944ac608f04c
RadoslawAdamczyk2000 <greatpolandweb@gmail.com>
2023-11-22 20:59:38 +0100

    work with device frameset

--- dev4/src/app/page.tsx
+++ dev4/src/app/page.tsx
@@ -3,7 +3,20 @@ import Hero from "pages/HomePage/Hero";
 export default function HomePage() {
 	return (
 		<div>
-			<Hero />
+			<Hero
+				buttons={[
+					{
+						label: "Blog",
+						uri: "#",
+					},
+					{
+						label: "Kontakt",
+						uri: "#",
+					},
+				]}
+				slogan="Odkryj internet<br/>na nowo"
+				title="Rad<br/>WEB"
+			/>
 		</div>
 	);
 }
--- dev4/src/components/Pages/HomePage/Hero/Hero.tsx
+++ dev4/src/components/Pages/HomePage/Hero/Hero.tsx
@@ -1,25 +1,50 @@
 import HeroButton from "u_buttons/HeroButton";
+import { DeviceFrameset } from "react-device-frameset";
+import "react-device-frameset/styles/marvel-devices.min.css";
 
-export default function Hero() {
+type tHero = {
+	buttons: NonNullable<{
+		label: string;
+		uri: string;
+	}>[];
+	slogan: string;
+	title: string;
+};
+
+export default function Hero(props: tHero) {
 	return (
 		<div>
 			<div>
 				<header>
-					<h1>
-						Rad
-						<br />
-						WEB
-					</h1>
-					<h2>
-						Odkryj internet
-						<br />
-						na nowo
-					</h2>
+					<h1 dangerouslySetInnerHTML={{ __html: props.title }} />
+					<h2 dangerouslySetInnerHTML={{ __html: props.slogan }} />
 				</header>
 				<ul>
-					<HeroButton label="Blog" uri="#" />
-					<HeroButton label="Kontakt" uri="#" />
+					{props.buttons?.map((item, index) => {
+						return <HeroButton key={index} label={item.label} uri={item.uri} />;
+					})}
 				</ul>
+			</div>
+			<div>
+				<DeviceFrameset device="MacBook Pro" color="silver">
+					<div
+						style={{
+							width: "100%",
+							height: 0,
+							paddingBottom: "72%",
+							position: "relative",
+						}}
+					>
+						<iframe
+							src="https://giphy.com/embed/JqmupuTVZYaQX5s094"
+							width="100%"
+							height="100%"
+							style={{ position: "absolute" }}
+							className="giphy-embed"
+							allowFullScreen
+						/>
+					</div>
+				</DeviceFrameset>
 			</div>
 		</div>
 	);
