b2bfd0f1d84ed40805522d0feee6bbe6ba54bbff
Radosław Adamczyk <84144731+RadWEB2000@users.noreply.github.com>
2023-11-29 19:58:19 +0100

    configure tsconfig

--- dev8/src/app/layout.tsx
+++ dev8/src/app/layout.tsx
@@ -1,6 +1,5 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="pl">
       <body className={inter.className}>{children}</body>
     </html>
   )
