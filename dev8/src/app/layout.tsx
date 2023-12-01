0a340db9ed020fa26dc624b9a5badea480ecac40
Radosław Adamczyk <84144731+RadWEB2000@users.noreply.github.com>
2023-12-01 20:43:05 +0100

    change background of body

--- dev8/src/app/layout.tsx
+++ dev8/src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pl">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className} >{children}</body>
     </html>
   )
 }
