e5403fb005d34eef18d34d5781e5b8f5ad9cd848
RadoslawAdamczyk2000 <greatpolandweb@gmail.com>
2024-01-13 08:56:53 +0100

    finish person hero section

--- dev2/src/components/Layout/Navigation/Navigation.tsx
+++ dev2/src/components/Layout/Navigation/Navigation.tsx
@@ -1,48 +1,51 @@
-"use client"
-import BigMenu from "l_nav/Menu/BigMenu";
-import { tBigMenu as tBigMenuData, tSocial} from "ts/types";
-import Brand from "l_nav/Brand";
-import Settings from "l_nav/Settings";
-import SmallMenu from "./Menu/SmallMenu/SmallMenu";
-import styles from "l_nav/Navigation.module.scss";
-import { useContext } from "react";
-import { LayoutContext } from "context/LayoutContext";
+'use client';
+import BigMenu from 'l_nav/Menu/BigMenu';
+import { tBigMenu as tBigMenuData, tSocial } from 'ts/types';
+import Brand from 'l_nav/Brand';
+import Settings from 'l_nav/Settings';
+import SmallMenu from './Menu/SmallMenu/SmallMenu';
+import styles from 'l_nav/Navigation.module.scss';
+import { useContext } from 'react';
+import { LayoutContext } from 'context/LayoutContext';
 
 type tNavigation = {
-    brand: {
-        label:string;
-        uri:string;
-    }
-     menu:{
-        big:tBigMenuData[];
-        small:{
-            label:string;
-            uri:string;
-        }[]
-     };
-     settings: {
-        menuButton:string;
-     };
-     socials:tSocial[];
-}
+  brand: {
+    label: string;
+    uri: string;
+  };
+  menu: {
+    big: tBigMenuData[];
+    small: {
+      label: string;
+      uri: string;
+    }[];
+  };
+  settings: {
+    menuButton: string;
+  };
+  socials: tSocial[];
+};
 
-export default function Navigation(props:tNavigation){
-    const {menu:{isOpen}} = useContext(LayoutContext);
-    return(
-        <nav className={styles.wrapper} data-expand={isOpen}>
-            <div className={styles.top}>
-                <Brand {...props.brand} />
-                <SmallMenu menu={props.menu.small} />
-                <Settings
-                    button={props.settings.menuButton}
-                />
-            </div>
-            <div className={styles.bottom}>
-                <BigMenu
-                    menu={props.menu.big}
-                    socials={props.socials}
-                />
-            </div>
-        </nav>
-    )
+export default function Navigation(props: tNavigation) {
+  const {
+    menu: { isOpen },
+  } = useContext(LayoutContext);
+  return (
+    <nav
+      className={styles.wrapper}
+      data-expand={isOpen}
+    >
+      <div className={styles.top}>
+        <Brand {...props.brand} />
+        <SmallMenu menu={props.menu.small} />
+        <Settings button={props.settings.menuButton} />
+      </div>
+      <div className={styles.bottom}>
+        <BigMenu
+          menu={props.menu.big}
+          socials={props.socials}
+        />
+      </div>
+    </nav>
+  );
 }
