4ca6a6d5484863555076258ceda4c889b0c35477
RadoslawAdamczyk2000 <greatpolandweb@gmail.com>
2024-01-13 21:05:46 +0100

    finish person hero, and continue work with article hero -  rwd

--- dev10/src/app/article/page.tsx
+++ dev10/src/app/article/page.tsx
@@ -1,35 +1,31 @@
-import { Hero } from "utils/Hero";
+import { Hero } from 'utils/Hero';
 
-export default function Page(){
-    return(
+export default function Page() {
+    return (
         <Hero
             breadcrumbs={[
                 {
-                    label:"Start",
-                    uri:"#"
+                    label: 'Start',
+                    uri: '#',
                 },
                 {
-                    label:"Blog",
-                    uri:"#"
+                    label: 'Blog',
+                    uri: '#',
                 },
                 {
-                    label:"Programowanie",
-                    uri:"#"
+                    label: 'Programowanie',
+                    uri: '#',
                 },
             ]}
             category={{
-                label:"Strony internetowe",
-                uri:"#"
+                label: 'Strony internetowe',
+                uri: '#',
             }}
-            image={{
-                altText:"",
-                sourceUrl:"https://images.unsplash.com/photo-1704996440137-44a1eb3c71ee?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                title:""
-            }}
-            matter="ads"
+            excerpt={`Molestiae beatae alias repellendus blanditiis doloremque cumque cupiditate inventore voluptatem. Amet beatae sit. Velit sequi eveniet et nihil tenetur temporibus exercitationem. At expedita et id. Id repudiandae dicta totam. Quia recusandae et est excepturi accusamus dignissimos quia dolor tenetur.`}
+            matter="technology"
             release="Wed May 29 2024 23:40:38 GMT+0200 (Central European Summer Time)"
             title="Jak wybrać odpowiednią bazę danych do projektu?"
             theme="blog"
         />
-    )  
+    );
 }
--- dev10/src/components/Utils/Hero/Hero.tsx
+++ dev10/src/components/Utils/Hero/Hero.tsx
@@ -1,163 +1,154 @@
-import getDate from "functions/getDate";
-import Image from "next/image";
-import Link from "next/link";
-import { tImage } from "ts/types";
-import { LinkButton } from "utils/Buttons";
-import css from "utils/Hero/Hero.module.scss";
+import getDate from 'functions/getDate';
+import Image from 'next/image';
+import Link from 'next/link';
+import { tImage } from 'ts/types';
+import { LinkButton } from 'utils/Buttons';
+import css from 'utils/Hero/Hero.module.scss';
+import { IoTimeOutline } from 'react-icons/io5';
+import { MdOutlineDateRange } from 'react-icons/md';
 
-type tHero = {
-} & (
-    {
-        theme:"home"
-    } & tHome |
-    {
-        theme:"person"
-    } & tPerson | 
-    {
-        theme:"blog"
-    } & tArticle | 
-    {
-        theme:"other"
-    } 
-)
+type tHero = {} & (
+    | ({
+          theme: 'home';
+      } & tHome)
+    | ({
+          theme: 'person';
+      } & tPerson)
+    | ({
+          theme: 'blog';
+      } & tArticle)
+    | {
+          theme: 'other';
+      }
+);
 
 type tArticle = {
-    breadcrumbs:{
-        label:string;
-        uri:string;
+    breadcrumbs: {
+        label: string;
+        uri: string;
     }[];
-    title:string;
-    release:string;
-    image:tImage;
-    matter:"seo"|"technology"|"ads"|"socials";
-    category:{
-        label:string;
-        uri:string;
-    }
-}
-
+    title: string;
+    release: string;
+    excerpt: string;
+    matter: 'seo' | 'technology' | 'ads' | 'socials' | 'www';
+    category: {
+        label: string;
+        uri: string;
+    };
+};
 
 type tHome = {
-    buttons?:{
-        label:string;
-        uri:string;
+    buttons?: {
+        label: string;
+        uri: string;
     }[];
-    movie:string;
-    slogan:string;
-    title:string;
-}
+    movie: string;
+    slogan: string;
+    title: string;
+};
 
 type tPerson = {
-    content:string;
-    image:tImage;
-    socials:{
-        icon:React.ReactNode;
-        label:string;
-        name:string;
-        uri:string;
+    content: string;
+    image: tImage;
+    socials: {
+        icon: React.ReactNode;
+        label: string;
+        name: string;
+        uri: string;
     }[];
-    title:string;
-    work:string;
-}
+    title: string;
+    work: string;
+};
 
-function Article(props:tArticle){
-    const {day,month,year} = getDate(props.release, "short");
-    return(
+function Article(props: tArticle) {
+    const { day, month, year } = getDate(props.release, 'short');
+    return (
         <header className={css.article__wrapper} data-category={props.matter}>
-            <ul className={css.article__breadcrumbs} >
-                {props.breadcrumbs.map((item,index) => {
-                    if(index != props.breadcrumbs.length -1){
-                        return(
-                            <Link
-                                className={css.article__breadcrumbs__item} 
-                                href={item.uri}
-                            >
+            <ul className={css.article__breadcrumbs}>
+                {props.breadcrumbs.map((item, index) => {
+                    if (index == props.breadcrumbs.length - 1) {
+                        return (
+                            <Link className={css.article__breadcrumbs__item} href={item.uri}>
                                 {item.label}
                             </Link>
-                        )
-                    }else{
-                        return(
+                        );
+                    } else {
+                        return (
                             <>
-                                <Link
-                                    className={css.article__breadcrumbs__item} 
-                                    href={item.uri}
-                                >
+                                <Link className={css.article__breadcrumbs__item} href={item.uri}>
                                     {item.label}
                                 </Link>
-                                <span className={css.article__breadcrumbs__divider} >/</span>
+                                <span className={css.article__breadcrumbs__divider}>/</span>
                             </>
-                        )
+                        );
                     }
                 })}
             </ul>
-            <section className={css.article__content} >
-                <p className={css.article__release} >{`${day} ${month} ${year}`}</p>
-                <h1 className={css.article__title}  dangerouslySetInnerHTML={{__html:props.title}} />
-                <Link href={props.category.uri} >{props.category.label}</Link>
-            </section>
-            <div className={css.article__image__wrapper} >
-                <figure className={css.article__image} >
-                    <Image
-                        alt={props.image.altText}
-                        fill
-                        loading="eager"
-                        src={props.image.sourceUrl}
-                        style={{
-                            objectFit:"cover",
-                            objectPosition:"center"
-                        }}
-                        title={props.image.title}
-                    />
-                </figure>
+            <h1 className={css.article__title} dangerouslySetInnerHTML={{ __html: props.title }} />
+            <Link className={css.article__category} href={props.category.uri}>
+                {props.category.label}
+            </Link>
+            <p
+                className={css.article__excerpt}
+                dangerouslySetInnerHTML={{ __html: props.excerpt }}
+            />
+            <div className={css.article__details}>
+                <p className={css.article__reading}>
+                    <b>
+                        <IoTimeOutline />
+                    </b>
+                    <span>17 min</span>
+                </p>
+                <p className={css.article__release}>
+                    <b>
+                        <MdOutlineDateRange />
+                    </b>
+                    <span> {`${day} ${month()} ${year}`}</span>
+                </p>
             </div>
         </header>
-    )
+    );
 }
 
-
-function Home(props:tHome){
-    return(
+function Home(props: tHome) {
+    return (
         <div className={css.home__wrapper}>
-                <video
-                    autoPlay
-                    className={css.home__video}
-                    loop
-                    muted
-                >
-                    <source src={props.movie} type="video/webm" />
-                </video>
+            <video autoPlay className={css.home__video} loop muted>
+                <source src={props.movie} type="video/webm" />
+            </video>
             <header className={css.home__content}>
-                <h1 className={css.home__title} dangerouslySetInnerHTML={{__html:props.title}} />
-                <h2 className={css.home__slogan} dangerouslySetInnerHTML={{__html:props.slogan}} />
-                {
-                    props.buttons &&
+                <h1 className={css.home__title} dangerouslySetInnerHTML={{ __html: props.title }} />
+                <h2
+                    className={css.home__slogan}
+                    dangerouslySetInnerHTML={{ __html: props.slogan }}
+                />
+                {props.buttons && (
                     <ul className={css.home__buttons}>
-                        {props.buttons.map((item,index) => {
-                            return(
-                                <LinkButton
-                                    {...item}
-                                    key={index}
-                                    theme="tertiary"
-                                />
-                            )
+                        {props.buttons.map((item, index) => {
+                            return <LinkButton {...item} key={index} theme="tertiary" />;
                         })}
                     </ul>
-                }
+                )}
             </header>
         </div>
-    )
+    );
 }
 
-
-function Person(props:tPerson){
-    return(
+function Person(props: tPerson) {
+    return (
         <div className={css.person__wrapper}>
             <header className={css.person__title__wrapper}>
                 <h1 className={css.person__title}>{props.title}</h1>
             </header>
             <section className={css.person__box}>
-                <h2 className={css.person__box__work} dangerouslySetInnerHTML={{__html:props.work}} />
-                <p className={css.person__box__content} dangerouslySetInnerHTML={{__html:props.content}} />
+                <h2
+                    className={css.person__box__work}
+                    dangerouslySetInnerHTML={{ __html: props.work }}
+                />
+                <p
+                    className={css.person__box__content}
+                    dangerouslySetInnerHTML={{ __html: props.content }}
+                />
             </section>
             <figure className={css.person__image}>
                 <Image
@@ -166,25 +157,22 @@ function Person(props:tPerson){
                     loading="eager"
                     src={props.image.sourceUrl}
                     style={{
-                        objectFit:"cover",
-                        objectPosition:"center"
+                        objectFit: 'cover',
+                        objectPosition: 'center',
                     }}
                     title={props.image.title}
                 />
             </figure>
         </div>
-    )
+    );
 }
 
-
-
-
-export default function Hero(props:tHero){
-    if(props.theme === "home") {
+export default function Hero(props: tHero) {
+    if (props.theme === 'home') {
         return <Home {...props} />;
-    }else if(props.theme === "person"){
+    } else if (props.theme === 'person') {
         return <Person {...props} />;
-    }else if(props.theme === "blog"){
-        return <Article {...props} />
+    } else if (props.theme === 'blog') {
+        return <Article {...props} />;
     }
 }
