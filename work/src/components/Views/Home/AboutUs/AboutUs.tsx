849f172c889d9f49123831880b7c9e95309c9177
RadoslawAdamczyk2000 <greatpolandweb@gmail.com>
2024-03-21 23:35:48 +0100

    m

--- work/src/components/Views/Home/AboutUs/AboutUs.tsx
+++ work/src/components/Views/Home/AboutUs/AboutUs.tsx
@@ -1,29 +1,69 @@
-import PrimaryButton from "buttons/PrimaryButton";
-import { PrimaryPersonCard } from "cards/index";
-import Image from "next/image";
-import { tAboutUs } from "v/home/AboutUs/AboutUs.models";
-import css from "v/home/AboutUs/AboutUs.module.scss";
+import PrimaryButton from 'buttons/PrimaryButton';
+import { PrimaryPersonCard } from 'cards/index';
+import Image from 'next/image';
+import { tAboutUs } from 'v/home/AboutUs/AboutUs.models';
+import css from 'v/home/AboutUs/AboutUs.module.scss';
 
-export default function AboutUs({button,content,image,team,title,uri}:tAboutUs) {
+export default function AboutUs({
+	button,
+	content,
+	image,
+	team,
+	title,
+	uri,
+}: tAboutUs) {
 	return (
-		<>	
-			D
-			{
-				team &&
+		<>
+			<section className={css.container}>
+				<h2 className={css.title}>{title}</h2>
+				<div className={css.button}>
+					<PrimaryButton
+						{...button}
+						mode="white"
+						rel="index follow"
+					/>
+				</div>
+				<p
+					className={css.content}
+					dangerouslySetInnerHTML={{ __html: content }}
+				/>
+				<figure className={css.image}>
+					<Image
+						alt={image.altText}
+						fill
+						loading="lazy"
+						src={image.sourceUrl}
+						style={{
+							objectFit: 'cover',
+							objectPosition: 'center',
+						}}
+						title={image.title}
+						quality={55}
+					/>
+				</figure>
+			</section>
+			{team && (
 				<ul className={css.cards}>
-					{team.reverse().map(({firstName,lastName, image , overview,works},index) => {
-						return (	
-							<PrimaryPersonCard
-								fullname={`${firstName} ${lastName}`}
-								image={image}
-								key={index}
-								overview={overview}
-								works={works}
-							/>
-						)
-					})}
+					{team
+						.reverse()
+						.map(
+							(
+								{ firstName, lastName, image, overview, works },
+								index
+							) => {
+								return (
+									<PrimaryPersonCard
+										fullname={`${firstName} ${lastName}`}
+										image={image}
+										key={index}
+										overview={overview}
+										works={works}
+									/>
+								);
+							}
+						)}
 				</ul>
-			}
+			)}
 		</>
 	);
 }
--- work/src/components/Views/Home/ServiceOverview/ServiceOverview.tsx
+++ work/src/components/Views/Home/ServiceOverview/ServiceOverview.tsx
@@ -1,34 +1,45 @@
-import Image from "next/image";
-import {tServiceOverview} from "v/home/ServiceOverview/ServiceOverview.models";
-import css from "v/home/ServiceOverview/ServiceOverview.module.scss";
+import Image from 'next/image';
+import { tServiceOverview } from 'v/home/ServiceOverview/ServiceOverview.models';
+import css from 'v/home/ServiceOverview/ServiceOverview.module.scss';
 
-export default function ServiceOverview({cards,title}:tServiceOverview) {
-    return (
-        <section className={css.wrapper} >
-            <h3 className={css.title}  dangerouslySetInnerHTML={{__html:title}} />
-            <ul className={css.cards} >
-                {cards.map(({icon,title}, index) => {
-                    return (
-                        <li className={css.card}  key={index}>
-                            <picture className={css.icon} >
-                                <Image
-                                    alt={icon.altText}
-                                    fill
-                                    loading="lazy"
-                                    src={icon.sourceUrl}
-                                    style={{
-                                        objectFit: 'contain',
-                                        objectPosition: 'center',
-                                    }}
-                                    title={icon.title}
-                                    quality={80}
-                                />
-                            </picture>
-                            <h4 className={css.label}  dangerouslySetInnerHTML={{__html:title}} />
-                        </li>
-                    )
-                })}
-            </ul>
-        </section>
-    )
+export default function ServiceOverview({ cards, title }: tServiceOverview) {
+	return (
+		<section className={css.wrapper}>
+			<h3
+				className={css.title}
+				dangerouslySetInnerHTML={{ __html: title }}
+			/>
+			<ul className={css.cards}>
+				{cards.map(({ icon, title }, index) => {
+					console.log('icon title', title);
+					return (
+						<li
+							aria-label={title}
+							className={css.card}
+							key={index}
+						>
+							<picture className={css.icon}>
+								<Image
+									alt={icon.altText}
+									fill
+									loading="lazy"
+									src={icon.sourceUrl}
+									style={{
+										objectFit: 'contain',
+										objectPosition: 'center',
+									}}
+									title={title}
+									quality={80}
+								/>
+							</picture>
+							<h4
+								className={css.label}
+								dangerouslySetInnerHTML={{ __html: title }}
+							/>
+						</li>
+					);
+				})}
+			</ul>
+		</section>
+	);
 }
